c2548994bd1fab83094cd860438ba3f6fa9cfdd2
w-ambura <160280733+w-ambura@users.noreply.github.com>
2024-02-24 23:53:07 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Prompting the user for their name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+# Printing out a personalized message using the user's input
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
